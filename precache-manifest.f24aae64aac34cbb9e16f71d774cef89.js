c3301a4ab114a1ee54fd0c07994314dcd713d6a3
Jina Zhu <jinazhu87@gmail.com>
2020-08-05 09:42:57 -0700

    Updates

--- precache-manifest.f24aae64aac34cbb9e16f71d774cef89.js
+++ precache-manifest.f24aae64aac34cbb9e16f71d774cef89.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "478e52cb2b734b67870077ef67e746a3",
+    "revision": "19ae67e3da9f3b3b65ee9ceee01f42f3",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "dd3bc28de224cf59d0d5",
+    "revision": "eb94e937014e1c01b28d",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd3bc28de224cf59d0d5",
-    "url": "/portfolio/static/js/main.4a82fe54.chunk.js"
+    "revision": "eb94e937014e1c01b28d",
+    "url": "/portfolio/static/js/main.40051096.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.725f7ef7486c1e0d6c72a6ef6a932175.js"
+  "/portfolio/precache-manifest.f24aae64aac34cbb9e16f71d774cef89.js"
 );
 
 self.addEventListener('message', (event) => {

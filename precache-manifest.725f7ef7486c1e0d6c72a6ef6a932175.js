4d265066b14b6c8437f106450dd66d912d6ec5c4
Jina Zhu <jinazhu87@gmail.com>
2020-08-05 09:36:37 -0700

    Updates

---
+++ precache-manifest.725f7ef7486c1e0d6c72a6ef6a932175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478e52cb2b734b67870077ef67e746a3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd3bc28de224cf59d0d5",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3bc28de224cf59d0d5",
+    "url": "/portfolio/static/js/main.4a82fe54.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);

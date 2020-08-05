99cef1307ad31a7a58f5afed9613eaf96b7e0f9a
Jina Zhu <jinazhu87@gmail.com>
2020-08-05 09:54:06 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Jina</h1>
+      <h1>Hello Sean</h1>
+      <p>This is my first portfolio</p>
     </div>
   );
 }

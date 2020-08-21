0575f17c59ff07ffbc463ee82f70f8bdbba5ca7b
Jina Zhu <jinazhu87@gmail.com>
2020-08-20 20:22:26 -0700

    create project page layout

---
+++ src/Project/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ProjectPage = styled.div`
+  background-color: #748173;
+  min-height: 100vh;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  margin: 1rem 5rem;
+`;
+
+export const Title = styled.h1`
+  font-family: Oswald;
+  margin: 0;
+  font-size: 50px;
+`;

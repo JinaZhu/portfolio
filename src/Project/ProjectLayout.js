4fcec27ae934ae4bfd6fafc4d446266054e038b6
Jina Zhu <jinazhu87@gmail.com>
2020-08-21 20:20:01 -0700

    add project container

--- src/Project/ProjectLayout.js
+++ src/Project/ProjectLayout.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+
+import githubLogo from "../images/Icons/github.svg";
 
 const ProjectLayout = () => {
-    return (
+  return (
+    <div>
+      <h2>Adventure Await</h2>
+      <div>
+        <p>
+          Adventure Awaits is a full-stack web application that allows anyone to
+          discover somewhere new by providing an insight into a country.
+        </p>
+        <div>
+          <p>React</p>
+          <p>Flask</p>
+          <p>Styled Components</p>
+        </div>
+        <a
+          href="https://github.com/JinaZhu/World-Traveler"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubLogo} alt="github logo" width="50" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
+export default ProjectLayout;
--- src/Project/index.js
+++ src/Project/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { SubTitle, ProjectPage, TitleContainer, Title } from "./styled";
 import Navbar from "../Navbar";
+import ProjectLayout from "./ProjectLayout";
 
 const Project = () => {
   return (
@@ -12,6 +13,7 @@ const Project = () => {
         <Title>I've Built</Title>
         <SubTitle>featured project</SubTitle>
       </TitleContainer>
+      <ProjectLayout />
     </ProjectPage>
   );
 };

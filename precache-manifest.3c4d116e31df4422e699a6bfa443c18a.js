203cd071d62c93558feeda2acfdd10a12d1006f7
Jina Zhu <jinazhu87@gmail.com>
2020-08-23 18:39:02 -0700

    Updates

---
+++ precache-manifest.3c4d116e31df4422e699a6bfa443c18a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fa4ad017c064e468d28cef7541b8b0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f847d3168a1caa275d71",
+    "url": "/portfolio/static/css/main.d46866f6.chunk.css"
+  },
+  {
+    "revision": "6ef6285cd6059671f9e2",
+    "url": "/portfolio/static/js/2.92202935.chunk.js"
+  },
+  {
+    "revision": "f5327a3e8204f21ff1b2e5af38bc59ba",
+    "url": "/portfolio/static/js/2.92202935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f847d3168a1caa275d71",
+    "url": "/portfolio/static/js/main.cf72b238.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6fc53e2d481ea7279dac0c78544dab83",
+    "url": "/portfolio/static/media/aa-homepage.6fc53e2d.png"
+  },
+  {
+    "revision": "e43f6f5c78c99a0f503bd4ecf1a5ab5b",
+    "url": "/portfolio/static/media/aa-map.e43f6f5c.png"
+  },
+  {
+    "revision": "eaa4e331f3ea82940b8339cf3947e699",
+    "url": "/portfolio/static/media/aa-pictures.eaa4e331.png"
+  },
+  {
+    "revision": "001dd7a00c9b30a0ee416884ded5cb43",
+    "url": "/portfolio/static/media/aa-places-been.001dd7a0.png"
+  },
+  {
+    "revision": "ad04f7b0165728c3d74710d1330acc6a",
+    "url": "/portfolio/static/media/aa-travel-info.ad04f7b0.png"
+  },
+  {
+    "revision": "8e270c31d4559a13ca57e0ed4a1347de",
+    "url": "/portfolio/static/media/bunny-played.8e270c31.png"
+  },
+  {
+    "revision": "4693e2b33d6cf5ccc92b174ce059eb55",
+    "url": "/portfolio/static/media/bunny-unplayed.4693e2b3.png"
+  },
+  {
+    "revision": "4bfa290750a69d941d5da12693c1c1cc",
+    "url": "/portfolio/static/media/cake-played.4bfa2907.png"
+  },
+  {
+    "revision": "8da14c6cad26fefde9a8321bb6e408d7",
+    "url": "/portfolio/static/media/cake-unplayed.8da14c6c.png"
+  },
+  {
+    "revision": "06b7e22f6b4e888c0b24667ee3c244ff",
+    "url": "/portfolio/static/media/github-brands.06b7e22f.svg"
+  },
+  {
+    "revision": "6bd27253718c14ddff4c4a6977fa3adf",
+    "url": "/portfolio/static/media/github.6bd27253.svg"
+  },
+  {
+    "revision": "3c16460332701e02b5309690a27369c1",
+    "url": "/portfolio/static/media/instagram.3c164603.svg"
+  },
+  {
+    "revision": "c0587bafcc6e58ebee3bed793c25c2d6",
+    "url": "/portfolio/static/media/jinterest-more.c0587baf.png"
+  },
+  {
+    "revision": "abbf81654c974682ea170aa5f30ab2bd",
+    "url": "/portfolio/static/media/jinterest.abbf8165.png"
+  },
+  {
+    "revision": "f171ad15f105ed42e0709e037bdd16b2",
+    "url": "/portfolio/static/media/leaf-branch.f171ad15.png"
+  },
+  {
+    "revision": "81198e0a77d2c9a0cd2564d6588ed98a",
+    "url": "/portfolio/static/media/link.81198e0a.svg"
+  },
+  {
+    "revision": "38c6146f7dfc4f533f9442ce864f7741",
+    "url": "/portfolio/static/media/linkedin.38c6146f.svg"
+  },
+  {
+    "revision": "ac2f3ee6be6aba74e7b2708903120f02",
+    "url": "/portfolio/static/media/qrzn-archive.ac2f3ee6.png"
+  },
+  {
+    "revision": "7702c73f786078749d6ba20d1c3280e0",
+    "url": "/portfolio/static/media/qrzn-cover.7702c73f.png"
+  },
+  {
+    "revision": "9df31546019904e8ed00c9dcff1647eb",
+    "url": "/portfolio/static/media/qzrn-awesome-graphic.9df31546.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.49f1ee9bc56cefde0b6e801e0ae8a399.js"
+  "/portfolio/precache-manifest.3c4d116e31df4422e699a6bfa443c18a.js"
 );
 
 self.addEventListener('message', (event) => {

92adb8e4a0d086451ef3713cc48619a69618c761
Jina Zhu <jinazhu87@gmail.com>
2021-01-26 14:37:33 -0800

    reduce animation for background flakes, add another snowflake, reduce image size

--- src/Homepage/Winter.js
+++ src/Homepage/Winter.js
@@ -9,6 +9,7 @@ import {
 import snowflake1 from "../images/Seasons/snowflake_1.png";
 import snowflake2 from "../images/Seasons/snowflake_2.png";
 import snowflake3 from "../images/Seasons/snowflake_3.png";
+import snowflake4 from "../images/Seasons/snowflake_4.png";
 
 const lefts = [
   "0%",
@@ -26,7 +27,7 @@ const lefts = [
 
 const durations = ["10s", "11s", "12s", "13s", "14s", "15s", "16s", "17s"];
 const delays = ["-10s", "-8s", "-6s", "-4s", "-2s", "0", "0", "0"];
-const snowflakes = [snowflake1, snowflake2, snowflake3];
+const snowflakes = [snowflake1, snowflake2, snowflake3, snowflake4];
 const sizes = [
   ["35", "0px"],
   ["50", "1px"],
@@ -56,7 +57,7 @@ const Winter = () => {
   return (
     <WinterContainer>
       <SnowflakeForward>
-        {Array(...Array(40)).map((_, i) => {
+        {Array(...Array(35)).map((_, i) => {
           const styles = randomizeStyles();
           return (
             <SnowflakeImg
@@ -74,7 +75,7 @@ const Winter = () => {
         })}
       </SnowflakeForward>
       <SnowflakeBackward>
-        {Array(...Array(25)).map((_, i) => {
+        {Array(...Array(20)).map((_, i) => {
           const styles = randomizeStyles();
           return (
             <SnowflakeImg
@@ -86,6 +87,7 @@ const Winter = () => {
               blur={"3px"}
               alt="snowflake"
               width="25"
+              fall={true}
             />
           );
         })}
